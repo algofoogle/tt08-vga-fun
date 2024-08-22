b74a5756bd14c5ecbbe1e4ba83d6bf14d3abaf9b
Anton Maurovic <anton@foogle.com>
2024-08-23 04:55:26 +0930

    Minor update

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -2,8 +2,13 @@ set layout [readnet spice $project.lvs.spice]
 set source [readnet spice /dev/null]
 readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $source
 
+#NOTE: For simplicity, ALL of the files below can be loaded for every
+# LVS run, because even if you're LVSing just one block, the last
+# 'lvs' command selects which one actually matters.
+
 # Add spice files of analog block(s):
 readnet spice ../xschem/simulation/csdac_nom.spice $source
+#readnet spice ../xschem/simulation/csdac_bare.spice $source
 
 # Add GL verilog of digital block(s) (i.e. flat file from OpenLane hardening):
 readnet verilog ../verilog/gl/controller.v $source
